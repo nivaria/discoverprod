fb3082a1957723137db23bfe89a434c3cce5f896
Kaloyan Petrov <kaloyan@designolog.com>
2013-05-23 16:59:31 +0100

    hide printing of fields not used

--- templates/nodes/node--restaurant-display.tpl.php
+++ templates/nodes/node--restaurant-display.tpl.php
@@ -153,14 +153,10 @@
   <div class="node-bottom">
     <?php
       // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
+      // hide($content['comments']);
+      // hide($content['links']);
+      // print render($content);
     ?>
-
-    <?php print render($content['links']); ?>
-
-    <?php print render($content['comments']); ?>
   </div>
 
 </article><!-- /.node -->
