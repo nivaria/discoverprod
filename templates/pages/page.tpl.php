256044f34ac84bb7fa41cd09b768c727ac3e1051
Kaloyan Petrov <kaloyan@designolog.com>
2013-05-27 10:59:30 +0100

    change in page template

--- templates/pages/page.tpl.php
+++ templates/pages/page.tpl.php
@@ -61,19 +61,21 @@
   <div id="main">
 
     <section id="content" class="column" role="main">
-      <?php print render($page['highlighted']); ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="page--title title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
+      <div class="content-header">
+        <?php print render($page['highlighted']); ?>
+        <a id="main-content"></a>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+          <h1 class="page--title title" id="page-title"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php print $messages; ?>
+        <?php print render($tabs); ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+      </div>
       <div class="content">
         <?php print render($page['content']); ?>
       </div>
